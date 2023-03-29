250f847a6c82313e8b79f31bfb532367e5d0d52f
aduome <115729741+aduome@users.noreply.github.com>
2023-03-29 14:43:48 +0000

    Delete WORKDIR/var/www/html directory

--- WORKDIR/var/www/html/index.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); /* Starts the session */
-
-if(!isset($_SESSION['UserData']['Username'])){
-	header("location:login.php");
-	exit;
-}
-?>
-
-Congratulation! You have logged in successfully to Azubi Cloud Project Version 2. <a href="logout.php">Click here</a> to Logout.
