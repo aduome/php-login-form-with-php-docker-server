5090485b67806d1534efaf8f6746e3f5890bb5eb
aduome <daniel.agyeibanahene@gmail.com>
2023-03-29 21:27:10 +0000

    login.php updated with styles

--- login.php
+++ login.php
@@ -28,6 +28,104 @@
 <title>Azubi Cloud Project Version_2</title>
 <link href="./css/style.css" rel="stylesheet">
 </head>
+
+<styles>
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 1rem;
+	color: #333;
+	background-color: #e9fcfc;
+}
+
+a {
+	user-select: none;
+	color: #1155CC;
+	font-family: Verdana, Helvetica Neue, Helvetica, Arial, sans-serif;
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: underline;
+}
+
+h3 {
+	margin: 0;
+	padding: 1rem 0.5rem;
+}
+
+#frame {
+	text-align: center;
+	background-color: #eff0c4;
+	margin: 3rem auto;
+	padding: 1rem;
+	width: 450px;
+	border: 2px solid #EEE;
+}
+
+.button {
+	border: 1px solid;
+	box-shadow: 0 1px 0 rgba(0, 0, 0, 0.1);
+	cursor: pointer;
+	display: inline-block;
+	font-size: 0.9rem;
+	font-weight: 700;
+	line-height: 2.3;
+	margin-right: 0.5rem;
+	min-width: 4rem;
+	padding: 0 1rem;
+	text-align: center;
+	user-select: none;
+	color: #FFF;
+	box-shadow: inset 0 1px 0 #529E21,
+		inset 1px 0 0 #4A911C,
+		inset -1px 0 0 #4A911C,
+		inset 0 -1px 0 #3D7F12,
+		inset 0 2px 0 rgba(255, 255, 255, 0.3),
+		inset 0 0 4px rgba(255, 255, 255, 0.3),
+		0 1px 2px rgba(0, 0, 0, 0.3);
+	background: #6dbd45;
+	background: linear-gradient(to bottom, #6dbd45 20%, #51a326 100%);
+	border: 0;
+	border-radius: 3px;
+	padding: 0.3rem 1rem;
+	text-align: center;
+	overflow: hidden;
+	min-width: 7rem;
+}
+
+.button:hover {
+	text-decoration: none;
+}
+
+.button:active {
+	background: #6dbd45;
+	background: linear-gradient(to bottom, #5EA534 20%, #519327 100%);
+	position: relative;
+	top: 1px;
+}
+.Input {
+	background-color: #FFFFFF;
+	border-color: #A0A0A0 #B9B9B9 #B9B9B9;
+	border-style: solid;
+	border-width: 1px;
+	box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1) inset;
+	font-family: "lucida grande", tahoma, verdana, arial, sans-serif;
+	font-size: 11px;
+	margin: 0;
+	padding: 5px;
+	width: 200px;
+}
+.Table {
+	font-family: verdana, Helvetica, sans-serif;
+	font-size: 12px;
+	color: #333;
+	background-color: #e4e4e4ce;
+}
+.Table td {
+	background-color: #b3fcd1;
+}
+
+</styles>
 <body>
 <div id="Frame0">
   <h1>Azubi Cloud Project Version 2</h1>
